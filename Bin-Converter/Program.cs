396c7d4e082076c3f18ce392875f8cc23b7830d8
Dziobax1 <58388062+Dziobax1@users.noreply.github.com>
2020-10-06 22:06:50 +0200

    Create Program.cs

---
+++ Bin-Converter/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BinConverter {
+    static class Program {
+        [STAThread]
+        static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Main());
+        }
+    }
+}
