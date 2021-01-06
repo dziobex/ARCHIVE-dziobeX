98432efde30d5d79533f088dc4223fddc8b8405c
dziobex <58388062+dziobex@users.noreply.github.com>
2021-01-06 16:00:28 +0100

    Add files via upload

---
+++ asciimovie/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ascii_movie {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Give a path to the file: ");
+            string path = Console.ReadLine();
+            Movie movie = new Movie(path);
+        }
+    }
+}
